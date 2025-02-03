2fa65e89eb416cf612c2be6dd1f81275f2603770
Mostafa Hosseini <54173709+mostafa1350@users.noreply.github.com>
2025-02-03 15:47:52 +0330

    create question in JS
    create question in JS

---
+++ js/Questions.js
@@ -0,0 +1,71 @@
+const questions=[
+    {
+        category:"programming",
+        questions:[
+            {
+                question:"what does HTM stand for?",
+                options:["Hyper Text","Hyper Text M L","Hyper MKL","Hyper Tools"],
+                correctAnsewer:1,
+            },
+            {
+                question:"what Function in JS?",
+                options:["func myFunction()","def myFunction()","create myFunction()","function myFunction()"],
+                correctAnsewer:3,
+            },
+
+    ]},
+
+    // ======================
+
+    {
+        category:"ٍEntomology",
+        questions:[
+            {
+                question:"what does mean Ptra in Classification?",
+                options:["Wing","Tarsus","Femur","Antenna"],
+                correctAnsewer:1,
+            },
+            {
+                question:"what is Facet in an insect",
+                options:["one part of a Compond Eyes","def myFunction()","create myFunction()","function myFunction()"],
+                correctAnsewer:0,
+            },
+
+    ]},
+
+    // ======================
+
+    {
+        category:"Math",
+        questions:[
+            {
+                question:"what does mean Power?",
+                options:["each number multiply to itself","Tarsus","Femur","Antenna"],
+                correctAnsewer:0,
+            },
+            {
+                question:"what is Facet in an insect?",
+                options:["func myFunction()","def myFunction()","create myFunction()","function myFunction()"],
+                correctAnsewer:3,
+            },
+
+    ]},
+        // ======================
+
+    {
+        category:"Physics",
+        questions:[
+            {
+                
+                question:"How many planets are there in our Galaxy?",
+                options:["8","9","10","7"],
+                correctAnsewer:1,
+            },
+            {
+                question:"what is Facet in an insect",
+                options:["func myFunction()","def myFunction()","create myFunction()","function myFunction()"],
+                correctAnsewer:3,
+            },
+
+    ]},
+];
