569db90d38980a02687d8485fff06507ba763fac
Mostafa Hosseini <54173709+mostafa1350@users.noreply.github.com>
2025-02-03 21:23:37 +0330

    Provide question for 4 parts
    Provide question for 4 parts

--- js/Questions.js
+++ js/Questions.js
@@ -3,14 +3,54 @@ const questions=[
         category:"programming",
         questions:[
             {
-                question:"what does HTM stand for?",
-                options:["Hyper Text","Hyper Text M L","Hyper MKL","Hyper Tools"],
+                question:"What tag is used to define a hyperlink in HTML?",
+                options:["<link>","<a>","<href>","<hyper>"],
                 correctAnsewer:1,
             },
             {
-                question:"what Function in JS?",
-                options:["func myFunction()","def myFunction()","create myFunction()","function myFunction()"],
+                question:"Which tag creates the largest heading in HTML?",
+                options:["<h6>","<h1>","<header>","<head>"],
+                correctAnsewer:1,
+            },
+            {
+                question:"What is the purpose of the <br> tag??",
+                options:["Start a new paragraph","Bold text","Create a list","Break line"],
                 correctAnsewer:3,
+            },
+            {
+                question:"Which HTML tag is used to define a table cell?",
+                options:["<table>","<td>","<th>","<tr>"],
+                correctAnsewer:0,
+            },
+            {
+                question:"What does the <ul> tag represent?",
+                options:["Ordered list","Underlined list","Unordered list","Universal list"],
+                correctAnsewer:0,
+            },
+            {
+                question:"How is a comment inserted in an HTML document?",
+                options:["<!-- comment -->","// comment //","/* comment */","#comment"],
+                correctAnsewer:0,
+            },
+            {
+                question:"Which attribute specifies the URL in an <a> tag for linking?",
+                options:["Link","src","href","url"],
+                correctAnsewer:2,
+            },
+            {
+                question:"What is a variable in Python?",
+                options:["A reserved word","A data type","A location in memory to store data","A function"],
+                correctAnsewer:2,
+            },
+            {
+                question:"Which of these is not a core data type in Python?",
+                options:["Lists","Dictionary","Tuple","Class"],
+                correctAnsewer:3,
+            },
+            {
+                question:"What data type is the object below ? li = [1, 23,'hello', 1]",
+                options:["Lists","Array","Dictionary","Tuple"],
+                correctAnsewer:0,
             },
 
     ]},
@@ -21,13 +61,53 @@ const questions=[
         category:"ٍEntomology",
         questions:[
             {
-                question:"what does mean Ptra in Classification?",
-                options:["Wing","Tarsus","Femur","Antenna"],
+                question:"Insects that pass through the egg, larva, pupa, and adult stages in their life cycle have....",
+                options:["Incomplete metamorphosis","No metamorphosis","Complete metamorphosis","Gradual metamorphosis"],
+                correctAnsewer:2,
+            },
+            {
+                question:"Axillary sclerites form points of attachment for muscles that control the:",
+                options:["Wings","Legs","Mouthparts","Antennae"],
+                correctAnsewer:0,
+            },
+            {
+                question:" .....is the stage of development of an insect between the immature, energy consuming form and the adult in holometabolous insects (those insects that go through complete metamorphosis).",
+                options:["Pupa","Larva","Imago","Third instar"],
+                correctAnsewer:0,
+            },
+            {
+                question:"Which structures would be found in an eruciform larva, but not in a scarabaeiform larva?",
+                options:["Prolegs","Manidibles","Spiracles","Ocelli"],
+                correctAnsewer:0,
+            },
+            {
+                question:"Which structure is NOT part of the head capsule of insects?",
+                options:["Antennae","Tentorium","Vertex","Pronotum"],
+                correctAnsewer:3,
+            },
+            {
+                question:"A legless, eyeless, vermiform larva that is approximately 5 mm in length would probably belong to what insect order?",
+                options:["Ephemeroptera","Orthoptera","Odonata","Siphonaptera"],
+                correctAnsewer:3,
+            },
+            {
+                question:"In cicadas and other sucking insects, movement of liquid to the mouth results from the action of a .....",
+                options:["Corneal pump","Corneagen cell","Cibarial pump","Cuticle"],
+                correctAnsewer:2,
+            },
+            {
+                question:"In centipedes, the 'fangs' are what type of structure?",
+                options:["Mandibles","Modified legs","Maxilla","Modified mouthparts"],
                 correctAnsewer:1,
             },
             {
-                question:"what is Facet in an insect",
-                options:["one part of a Compond Eyes","def myFunction()","create myFunction()","function myFunction()"],
+                question:"A ........ is the immature, aquatic stage of a dobsonfly.",
+                options:["Hellbender","Ant lion","Hellgrammite","Grub"],
+                correctAnsewer:2,
+            },
+            {
+                question:"If an insect's subesophageal ganglion were paralyzed, it would be unable to .....",
+                options:["Eat","Fly","Walks","See"],
                 correctAnsewer:0,
             },
 
@@ -36,16 +116,56 @@ const questions=[
     // ======================
 
     {
-        category:"Math",
+        category:"Geography",
         questions:[
             {
-                question:"what does mean Power?",
-                options:["each number multiply to itself","Tarsus","Femur","Antenna"],
+                question:"What is the name of the tallest mountain in the world?",
+                options:["K2","Everest","Makalu","Manaslu"],
+                correctAnsewer:1,
+            },
+            {
+                question:"Which of the following is a valid reason to try to collect data from a whole population rather than just a sample of one?",
+                options:["If the method is known to create biased results","If two populations are being compared with each other","If the population is very small","If the population is very large"],
+                correctAnsewer:2,
+            },
+            {
+                question:"What is the minimum sample size to carry out a Spearman's rank correlation coefficient test?",
+                options:["10 pairs of individuals","15 pairs of individuals","20 pairs of individuals","25 pairs of individuals"],
                 correctAnsewer:0,
             },
             {
-                question:"what is Facet in an insect?",
-                options:["func myFunction()","def myFunction()","create myFunction()","function myFunction()"],
+                question:"A sample should be ..... the population being observed.",
+                options:["the same size as","half the size of","distinctly different from","representative of"],
+                correctAnsewer:3,
+            },
+            {
+                question:"The capital of Fiji is:",
+                options:["Jakarta","Lima","Suva","Warsaw"],
+                correctAnsewer:2,
+            },
+            {
+                question:"Which of these cities was known as Philadelphia in ancient times?",
+                options:["Amman,Jordan","Athens,Greece","Tel Aviv, Israel","Damascus,Syria"],
+                correctAnsewer:0,
+            },
+            {
+                question:"Which peak is the highest active volcano in Europe?",
+                options:["Vesuvius","Mount Etna","Eyja","Klyuchevskaya"],
+                correctAnsewer:1,
+            },
+            {
+                question:"Svithiod is the ancient name of which country?",
+                options:["Switzerland","Somalia","Sweden","Suriname"],
+                correctAnsewer:2,
+            },
+            {
+                question:"The capital of Afghanistan is:",
+                options:["Kabul","Kampala","Tehran","Basseterre"],
+                correctAnsewer:0,
+            },
+            {
+                question:"Which of these nations does not border Thailand?",
+                options:["Myanmar","Laos","Cambodia","China"],
                 correctAnsewer:3,
             },
 
@@ -59,12 +179,52 @@ const questions=[
                 
                 question:"How many planets are there in our Galaxy?",
                 options:["8","9","10","7"],
+                correctAnsewer:0,
+            },
+            {
+                question:"A pull or a push applied or exerted on an object is ........",
+                options:["Energy","Force","Velocity","Distance"],
                 correctAnsewer:1,
             },
             {
-                question:"what is Facet in an insect",
-                options:["func myFunction()","def myFunction()","create myFunction()","function myFunction()"],
+                question:"...... is said to be done when a force of 1Newton moves an object of mass 1 gram, 1 meter in the direction of the force.",
+                options:["Work","Force","Energy","Time"],
+                correctAnsewer:0,
+            },
+            {
+                question:"Which of the following energy sources is characterized by the process of nuclear fusion?",
+                options:["Coal","Natural Gas","Geothermal","Solar"],
                 correctAnsewer:3,
+            },
+            {
+                question:"What does Newton's second law of motion state?",
+                options:["Force is directly proportional to mass times acceleration.","Force is inversely proportional to acceleration.","Force is directly proportional to mass.","Force is inversely proportional to mass."],
+                correctAnsewer:0,
+            },
+            {
+                question:"According to Boyle's Law, what happens to the volume of a gas if its pressure is increased while keeping the temperature constant?",
+                options:["Volume increases.","Volume decreases.","Volume remains the same.","Volume becomes zero."],
+                correctAnsewer:1,
+            },
+            {
+                question:"Charles' Law describes the relationship between the volume and temperature of a gas at constant pressure. What does the law state?",
+                options:["Volume is inversely proportional to temperature.","Volume is directly proportional to temperature.","Volume is inversely proportional to pressure.","Volume is directly proportional to pressure."],
+                correctAnsewer:1,
+            },
+            {
+                question:"Which of the following is a fundamental principle of classical mechanics that describes the relationship between force, mass, and acceleration?",
+                options:["Coulomb's Law","Kepler's Law","Newton's Second Law","Boyle's Law"],
+                correctAnsewer:2,
+            },
+            {
+                question:"In the context of basic physics, what principle states that energy cannot be created or destroyed, only transformed from one form to another?",
+                options:["Newton's Third Law","Conservation of Energy","Ohm's Law","Hooke's Law"],
+                correctAnsewer:1,
+            },
+            {
+                question:"We connected the two ends of the wire together and then connected the beginning and end to a battery. The current intensity in the upper wire relative to the lower wire:",
+                options:["It is more","It is less","It is equal","It depends on their resistance."],
+                correctAnsewer:2,
             },
 
     ]},
